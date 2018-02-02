1d66ca43b4e11c44426139ac1ebbb72e335a0e6a
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-02-02 22:16:17 +0100

    v0.1.1
    STRUCTURE
    + Added Jquery plugin for color animaiton
    + Created menu.js for menu script
    HTML
    + Added transition "hr" element
    + Added page content structure
    + Added additional title on theme window
    JAVASCRIPT
    + In loader.js modified way to hide loader and show theme window
    + In theme.js added visual interactions with arrow button
    + In theme.js added event handling for arrow button, and function that can change page content
    + In menu.js created standard script structure
    CSS
    + Modified theme style to handle new title element
    + Created style for transition "hr"
    + Created style for "page" section, and menu bar

--- js/theme.js
+++ js/theme.js
@@ -1,10 +1,27 @@
 "use strict";
 
 $(document).ready(function(){
-    
+    //title color hover
     $(".fThemeArrow").mouseenter(function(){
-        
-        
+        $(".fTheme h2").slideDown(300);
+    });
+    $(".fThemeArrow").mouseleave(function(){
+        $(".fTheme h2").slideUp(300);
+    });
+    
+    //onclick animations
+    $(".fThemeArrow").click(function(){
+        $(".fTransition").show();
+        $(".fTheme").animate({
+            top: "-110%"
+        },1000,"swing",function(){
+            $(".fTheme").hide();
+            $(".fPage").show().animate({
+                top: "0px"
+            },1000,"swing",function(){
+                $(".fTransition").hide();
+            });
+        });
         
     });
     
