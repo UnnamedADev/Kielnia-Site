d162c6c3989775cbdbb71c791dd6492afa74b246
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-02-02 18:41:49 +0100

    v0.0.4
    STRUCTURE
    + Added JQuery UI
    + Created theme.js
    + Added .mp4 background
    HTML
    + Added video/beta preloader
    + Added theme window content
    JAVASCRIPT
    + In loader.js added beta preloader video starter
    + In theme.js added basic code structure and mouse event for theme window arrow button

--- js/loader.js
+++ js/loader.js
@@ -1,20 +1,30 @@
 "use strict";
 
-$(document).ready(function(){
+//const
+const loadDelay = 0*1000;
 
+$(document).ready(function(){
+    
+    //main
     $(".fLoaderHolder").fadeIn(300, function(){
         $(".fLoaderAnimation").fadeIn(300,function(){
             $(".fLoaderHolder > h1").fadeIn(300, afterLoad);
         });
     });
     
+    //fx
     function afterLoad(){
-        setTimeout(function(){
-            $(".fLoader").slideUp(500, "linear");
-        },2000);
-        setTimeout(function(){
-            $(".fLoaderAfter").slideUp(400,"linear");
-        },2100);
-    }
 
+        setTimeout(function(){
+            $(".fLoader").slideUp(1000, "swing");
+        },loadDelay);
+        setTimeout(function(){
+            $(".fLoaderAfter").slideUp(1000,"swing");
+            afterOut();
+        },loadDelay+100);
+    }
+    
+    function afterOut(){
+        //$(".preLoader").trigger("play");
+    }
 });
---
+++ js/theme.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function(){
+    
+    $(".fThemeArrow").mouseenter(function(){
+        
+        
+        
+    });
+    
+});
