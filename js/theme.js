bee53f1f20c10d40becbb577d579b505ccdc6ebf
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-02-03 04:55:41 +0100

    v0.1.2
    HTML
    + Added menu structure
    JAVASCRIPT
    In theme.js changed additional interactive title animation
    In menu.js added code that can build menu
    + In menu.js added close button event handling
    + In menu.js added code that provides interactions for close button
    CSS
    + Created style for menu window
    + Changed style of theme window elements like titles and "arrow button"

--- js/theme.js
+++ js/theme.js
@@ -3,10 +3,12 @@
 $(document).ready(function(){
     //title color hover
     $(".fThemeArrow").mouseenter(function(){
-        $(".fTheme h2").slideDown(300);
+        $(".fTheme h2").slideDown(100);
+        $(".fTheme h1").slideUp(200);
     });
     $(".fThemeArrow").mouseleave(function(){
-        $(".fTheme h2").slideUp(300);
+        $(".fTheme h2").slideUp(100);
+        $(".fTheme h1").slideDown(200);
     });
     
     //onclick animations
@@ -14,11 +16,11 @@ $(document).ready(function(){
         $(".fTransition").show();
         $(".fTheme").animate({
             top: "-110%"
-        },1000,"swing",function(){
+        },400,"swing",function(){
             $(".fTheme").hide();
             $(".fPage").show().animate({
                 top: "0px"
-            },1000,"swing",function(){
+            },300,"swing",function(){
                 $(".fTransition").hide();
             });
         });
